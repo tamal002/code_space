483af432fb955ad075efdbb92c2c56f5bfd07c84
Tamal saha <128189375+tamal002@users.noreply.github.com>
2025-02-21 17:55:09 +0000

    graph/prims - optimized

--- graph/prims.cpp
+++ graph/prims.cpp
@@ -17,58 +17,64 @@ map<int, vector<pair<int, int>>> constructGraph(vector<vector<int>> &input)
     return graph;
 }
 
-map<int, vector<pair<int, int>>> primsMinSpanningTree(map<int, vector<pair<int, int>>> &graph){
+
+map<int, vector<pair<int, int>>> primsMinSpanningTree(map<int, vector<pair<int, int>>> &graph, int startNode){
     
-    // REQUIRED CONTAINERS
+    // CHECKING IF THE START_NODE IS ISOLATED OR NOT.
+    for(auto &i : graph[startNode]){
+        if(i.first == -1){
+            cout << "INVALID START NODE." << endl;
+            return {};
+        }
+    }
+    
+    //CHECKING IF THE NODE IS ACTUALLY PRESENT IN GRAPH OR NOT.
+    if(graph.find(startNode) == graph.end()){
+        cout << "INVALID START NODE." << endl;
+        return {};
+    }
+    
+    
+    // REQUIRED CONTAINERS.
     set<int> visit;
     map<int, vector<pair<int, int>>> ans;
-    set<pair<int, pair<int, int>>> s; 
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq; //[MIN_HEAP]
+    int minCost = 0;
     
-    // FINDING THE MINIMUM WEIGHTED EDGE.
-    int start = -1;
-    int dest = -1;
-    int cost = INT_MAX;
-    for(auto &i : graph){
-        for(auto &j : i.second){
-            if(j.second < cost){
-                cost = j.second;
-                start = i.first;
-                dest = j.first;
+    pq.push({0, {startNode, -1}});
+    
+    while(!pq.empty()){
+        int start = pq.top().second.first;
+        int dest = pq.top().second.second;
+        int cost = pq.top().first;
+        
+        pq.pop();
+        
+        if(dest == -1){
+            visit.insert(start);
+        
+            // PUSHING ALL THE EDGES ASSOCIATED WITH startNode
+            for(auto &i : graph[start]){
+                pq.push({i.second, {start, i.first}});
             }
         }
-    }
-    
-    ans[cost].push_back({start, dest});
-    visit.insert(start);
-    visit.insert(dest);
-    for(auto &i: graph){
-        if(i.first == start || i.first == dest){
-            for(auto &j : i.second){
-                s.insert({j.second, {i.first, j.first}});
-            }
-        }
-    }
-    
-    while(!s.empty()){
-        auto smallest = *s.begin();
-        start = smallest.second.first;
-        dest = smallest.second.second;
-        cost = smallest.first;
-        s.erase(s.begin());
-        if(visit.find(dest) != visit.end()) continue;
+        else if(visit.find(dest) != visit.end()) continue;
         else{
             for(auto &i : graph[dest]){
-                s.insert({i.second, {dest, i.first}});
+                pq.push({i.second, {dest, i.first}});
             }
-            ans[cost].push_back({start, dest});
+            ans[start].push_back({dest, cost});
+            minCost += cost;
             visit.insert(dest);
         }
-        
     }
     
+    
+    cout << "TOTAL MINIMUM COST : " << minCost << endl;
+    
     return ans;
+    
 }
-
 
 
 void printList(map<int, vector<pair<int, int>>> &ans){
@@ -80,7 +86,7 @@ void printList(map<int, vector<pair<int, int>>> &ans){
     cout << "TAKEN EDGES." << endl;
     for(auto &i : ans){
         for(auto &j : i.second){
-            cout << "edge between " << j.first << " and " << j.second << " => cost : " << i.first << endl;
+            cout << "edge between " << i.first << " and " << j.first << " => cost : " << j.second << endl;
         }
     }
 }
@@ -89,7 +95,7 @@ int main()
 {
     vector<vector<int>> input = {{1, 6, 10}, {6, 1, 10}, {6, 5, 25}, {5, 6, 25}, {5, 4, 22}, {4, 5, 22}, {4, 7, 18}, {7, 4, 18}, {7, 5, 24}, {5, 7, 24}, {7, 2, 14}, {2, 7, 14}, {1, 2, 28}, {2, 1, 28}, {2, 3, 16}, {3, 2, 16}, {3, 4, 12}, {4, 3, 12}};
     map<int, vector<pair<int, int>>> graph = constructGraph(input);
-    map<int, vector<pair<int, int>>> ans = primsMinSpanningTree(graph);
+    map<int, vector<pair<int, int>>> ans = primsMinSpanningTree(graph, 5);
     printList(ans);
     return 0;
 }
